079d7f626260ef332c3cf6e391797ecb43e3cd3a
Koshiro Miyauchi <60140464+miyalab@users.noreply.github.com>
2020-06-10 13:07:24 +0900

    Update switch-case.cpp

--- cpp/basic_grammar/switch-case.cpp
+++ cpp/basic_grammar/switch-case.cpp
@@ -1,1 +1,14 @@
-
+switch(pattern){
+  case 0:
+    // pattern == 0 のときに実行される処理;
+    break;
+    
+  case 1:
+  case 2:
+    // pattern == 1 もしくは pattern == 2のときに実行される処理
+    break;
+    
+  default:
+    // patternがどのcaseにも合致しなかった場合に実行される処理
+    break;
+}
