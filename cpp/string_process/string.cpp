ba7a0e201ae0224849c770b0ea9cfd5830e93866
Koshiro Miyauchi <60140464+miyalab@users.noreply.github.com>
2020-06-19 00:00:12 +0900

    Update string.cpp

--- cpp/string_process/string.cpp
+++ cpp/string_process/string.cpp
@@ -3,5 +3,6 @@ int main()
 {
   std::string str1;         // 文字列型の宣言
   std::string str2("abc");  // 文字列型の宣言して、「abc」で定義
-  std::string str3="efg";   // 文字列型の宣言して、「efg」で定義
+  std::string str3(5,'a');  // 文字列型の宣言して、'a'を5個格納したものを定義
+  std::string str4="efg";   // 文字列型の宣言して、「efg」で定義
 }
