e897efa7ce33d0cb95e23e559c55c9d2321f4e0b
Koshiro Miyauchi <60140464+miyalab@users.noreply.github.com>
2020-06-19 00:05:13 +0900

    Update str_cat.cpp

--- cpp/string_process/str_cat.cpp
+++ cpp/string_process/str_cat.cpp
@@ -3,6 +3,6 @@ int main()
 {
   std::string str1="abc";
   std::string str2="def";
-  std::string str3=str1+str2;
+  std::string str3=str1+str2;       // 「abcdef」がstr3に格納される。
   std::cout << str3 << std::endl;
 }
