f27410e8a00d37fc5acda3e7fb25346e28c193e5
Koshiro Miyauchi <60140464+miyalab@users.noreply.github.com>
2020-06-19 00:26:21 +0900

    Create substr.cpp

---
+++ cpp/string_process/substr.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+int main()
+{
+  std::string str="abcdefghijklmn";
+  
+  // 変数.substr(i,j);               // 変数のi+1文字目からj文字抽出
+  std::string str2=str.substr(1,3); // 「bcd」が抽出される
+  
+  // 変数.substr(i);                // 変数のi+1文字目以降を抽出
+  std::string str3=str.substr(5);   // 「fghijklmn」が抽出される
+  
+  std::cout << str2 << std::endl;
+  std::cout << str2 << std::endl;
+}
