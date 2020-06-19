2fbf646ca90c2c39aa4b7d5e8438bd02f5597a72
Koshiro Miyauchi <60140464+miyalab@users.noreply.github.com>
2020-06-19 20:40:20 +0900

    Update vector.cpp

--- cpp/array_dynamic/vector.cpp
+++ cpp/array_dynamic/vector.cpp
@@ -1,5 +1,12 @@
 #include <vector>
 int main()
 {
-
+  // vector<変数型> 変数名;
+  std::vector<int> vec1;        // int型の動的配列を宣言
+  std::vector<int> vec2(5);     // int型の動的配列を5つの要素を用意して宣言
+  std::vector<int> vec3(5,3);   // int型の動的配列を5つの要素を用意して、すべて3で初期化
+  
+  std::vector<std::vector<int>> vec4;                         // int型の2次元動的配列を宣言
+  std::vector<std::vector<int>> vec5(5,vector<int>(5));       // int型の2次元動的配列を5x5の要素を用意して宣言
+  std::vector<std::vector<int>> vec6(5,vector<int>(5,3));     // int型の2次元動的配列を5x5の要素を用意して、すべて3で初期化
 }
