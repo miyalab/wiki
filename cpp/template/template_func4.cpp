bf3d4228052bfbdbd1fadad096fb2c19e644278a
Koshiro Miyauchi <60140464+miyalab@users.noreply.github.com>
2020-06-21 01:55:05 +0900

    Create template_func4.cpp

---
+++ cpp/template/template_func4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+template<typename type>
+void print(type s)
+{
+  std::cout << "num:" << s << std::endl;
+}
+
+templete<>
+void print(std::string str)
+{
+  std::cout << "str:"+str << std::endl;
+}
+
+int main()
+{
+  int a=100;
+  double b=150;
+  std::string s="200";
+  print(a);             // num:100 と出力される
+  print(b);             // num:150 と出力される
+  print(s);             // str:200 と出力される
+}
