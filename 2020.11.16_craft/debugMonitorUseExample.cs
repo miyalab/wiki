f84f8b37a05f90609ebff8804c63f03b7796f56e
MiYA LAB <60140464+miyalab@users.noreply.github.com>
2020-11-16 06:03:04 +0900

    Update debugMonitorUseExample.cs

--- 2020.11.16_craft/debugMonitorUseExample.cs
+++ 2020.11.16_craft/debugMonitorUseExample.cs
@@ -8,11 +8,10 @@ namespace TestApp
     public partial class Form1 : Form
     {
         // インスタンス生成例
-        DebugMonitor debugMonitor = new DebugMonitor();
-        DebugMonitor debugMonitor1 = new DebugMonitor(100, 200);
-        DebugMonitor debugMonitor2 = new DebugMonitor(500, 500, 300, 300);
+        DebugMonitor debugMonitor = new DebugMonitor();                     // モニタ作成
+        DebugMonitor debugMonitor1 = new DebugMonitor(100, 200);            // モニタサイズ指定して作成
+        DebugMonitor debugMonitor2 = new DebugMonitor(500, 500, 300, 300);  // モニタサイズ、場所を指定して作成
         
-
         public Form1()
         {
             InitializeComponent();
@@ -21,26 +20,31 @@ namespace TestApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            debugMonitor.Show();
+            // モニタを隠す
+            debugMonitor.Hide();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // モニタサイズを変更する
             debugMonitor.ChangeWindowSize(200, 200);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // テキストを挿入
             debugMonitor.Write("button2 push!");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            // テキストを挿入し、改行
             debugMonitor.WriteLine("button3 push!");
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            // ウインドウ表示場所を変更する
             debugMonitor.MoveWindow(600, 0);
         }
     }
