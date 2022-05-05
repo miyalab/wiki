932f8396e72e1b9a9def76935154b0d80d9d0112
MiYA LAB <60140464+miyalab@users.noreply.github.com>
2022-05-05 18:20:00 +0900

    Update cin.cpp

--- cpp/console_process/cin.cpp
+++ cpp/console_process/cin.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 int main()
 {
-  int a,b,c;
+    int a,b,c;
   
-  // std::cin >> データを入れる変数;
-  std::cin >> a;
+    // std::cin >> データを入れる変数;
+    std::cin >> a;
   
-  // 「>>」で複数のデータ入力ができる
-  std::cin >> b >> c;
+    // 「>>」で複数のデータ入力ができる
+    std::cin >> b >> c;
 }
