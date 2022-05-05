59bfc5d27310ef669007aabf5e5912882fc6fd22
MiYA LAB <60140464+miyalab@users.noreply.github.com>
2022-05-05 18:18:36 +0900

    Update iostream_fast.cpp

--- cpp/console_process/iostream_fast.cpp
+++ cpp/console_process/iostream_fast.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 int main()
 {
-  // 「iostream」と「stdio」の関係をきる
-  std::ios::sync_with_stdio(false);
+    // 「iostream」と「stdio」の関係をきる
+    std::ios::sync_with_stdio(false);
   
-  // 「std::cin」の高速化
-  std::cin.tie(nullptr);
+    // 「std::cin」の高速化
+    std::cin.tie(nullptr);
 }
